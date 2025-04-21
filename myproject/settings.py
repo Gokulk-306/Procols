5f5e9d63245d1da65e093e79026ce39a372bc7fd
Procols <connect.procols@gmail.com>
2025-04-21 14:33:01 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u9#cy-d)70j&t#ss*010ehfqv1!eo38zs2a%k-qlin8%se92ex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'procols.onrender.com',
